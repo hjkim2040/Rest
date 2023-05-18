5eb56b8613647c30dd7fa777a0f1bdfbffba5dba
hjkim2040 <hjkim2661@gmail.com>
2023-05-18 13:37:23 +0900

    문서 꾸미기

--- src/main/java/com/ll/rest/boundedContext/member/controller/ApiV1MemberController.java
+++ src/main/java/com/ll/rest/boundedContext/member/controller/ApiV1MemberController.java
@@ -3,6 +3,8 @@ package com.ll.rest.boundedContext.member.controller;
 import com.ll.rest.base.rsData.RsData;
 import com.ll.rest.boundedContext.member.entity.Member;
 import com.ll.rest.boundedContext.member.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
@@ -20,6 +22,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/api/v1/member", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
+@Tag(name = "ApiV1MemberController", description = "로그인, 로그인 된 회원의 정보")
 public class ApiV1MemberController {
     private final MemberService memberService;
 
@@ -36,6 +39,7 @@ public class ApiV1MemberController {
         private final String accessToken;
     }
     @PostMapping("/login")
+    @Operation(summary = "로그인, 엑세스 토큰 발급")
     public RsData<LoginResponse> login(@Valid @RequestBody LoginRequest loginRequest) {
         String accessToken = memberService.genAccessToken(loginRequest.getUsername(), loginRequest.getPassword());
 
@@ -52,6 +56,7 @@ public class ApiV1MemberController {
         private final Member member;
     }
     @GetMapping(value = "/me", consumes = ALL_VALUE)
+    @Operation(summary = "로그인된 사용자의 정보")
     public RsData<MeResponse> me(@AuthenticationPrincipal User user) {
 
         Member member = memberService.findByUsername(user.getUsername()).get();
