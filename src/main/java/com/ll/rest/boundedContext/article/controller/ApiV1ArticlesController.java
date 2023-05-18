f563be62c00350769e28f5fe23640694d3cf36af
hjkim2040 <hjkim2661@gmail.com>
2023-05-18 14:40:59 +0900

    게시물 리스트

---
+++ src/main/java/com/ll/rest/boundedContext/article/controller/ApiV1ArticlesController.java
@@ -0,0 +1,37 @@
+package com.ll.rest.boundedContext.article.controller;
+
+import com.ll.rest.base.rsData.RsData;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/api/v1/articles", produces = APPLICATION_JSON_VALUE)
+@Tag(name = "ApiV1ArticlesController", description = "게시물 CRUD 컨트롤러")
+public class ApiV1ArticlesController {
+    private final ArticleService articleService;
+
+    @AllArgsConstructor
+    @Getter
+    public static class ArticlesResponse {
+        private final List<Article> articles;
+    }
+
+    @GetMapping(value = "")
+    @Operation(summary = "게시물들")
+    public RsData<ArticlesResponse> articles() {
+        List<Article> articles = articleService.findAll();
+
+        return RsData.of(
+                "S-1",
+                "성공",
+                new ArticlesResponse(articles)
+        );
+    }
+}
