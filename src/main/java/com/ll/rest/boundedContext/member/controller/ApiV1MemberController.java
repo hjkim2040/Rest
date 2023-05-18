c4661eab51d90bd49e59a06be3b061bd77f47f51
hjkim2040 <hjkim2661@gmail.com>
2023-05-18 10:07:28 +0900

    member/me

--- src/main/java/com/ll/rest/boundedContext/member/controller/ApiV1MemberController.java
+++ src/main/java/com/ll/rest/boundedContext/member/controller/ApiV1MemberController.java
@@ -1,6 +1,7 @@
 package com.ll.rest.boundedContext.member.controller;
 
 import com.ll.rest.base.rsData.RsData;
+import com.ll.rest.boundedContext.member.entity.Member;
 import com.ll.rest.boundedContext.member.service.MemberService;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
@@ -9,11 +10,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import static org.springframework.http.MediaType.ALL_VALUE;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
@@ -35,7 +34,7 @@ public class ApiV1MemberController {
         private final String accessToken;
     }
     @PostMapping("/login")
-    public RsData<LoginResponse> login(@Valid @RequestBody LoginRequest loginRequest, HttpServletResponse resp) {
+    public RsData<LoginResponse> login(@Valid @RequestBody LoginRequest loginRequest) {
         String accessToken = memberService.genAccessToken(loginRequest.getUsername(), loginRequest.getPassword());
 
 
@@ -43,6 +42,22 @@ public class ApiV1MemberController {
                 "S-1",
                 "엑세스토큰이 생성되었습니다.",
                 new LoginResponse(accessToken)
+        );
+    }
+    @AllArgsConstructor
+    @Getter
+    public static class MeResponse {
+        private final Member member;
+    }
+    @GetMapping(value = "/me", consumes = ALL_VALUE)
+    public RsData<MeResponse> me() {
+
+        Member member = memberService.findByUsername("user1").get();
+
+        return RsData.of(
+                "S-1",
+                "엑세스토큰이 생성되었습니다.",
+                new MeResponse(member)
         );
     }
 }
